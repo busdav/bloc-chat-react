ba7c456270577ca5a2108da084e46d00e854c8ac
David Buser <busdav@outlook.com>
2018-12-28 18:27:17 +0100

    Complete list chat rooms

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+import RoomList from './components/RoomList';
+import Landing from './components/Landing';
+import { Route, Link } from 'react-router-dom';
+
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCy0KAQzOEzy6vWw9aL9ZQkVJNkPW4L31M",
+  authDomain: "bloc-chat-react-1b9df.firebaseapp.com",
+  databaseURL: "https://bloc-chat-react-1b9df.firebaseio.com",
+  projectId: "bloc-chat-react-1b9df",
+  storageBucket: "bloc-chat-react-1b9df.appspot.com",
+  messagingSenderId: "85511703523"
+};
+firebase.initializeApp(config);
 
 class App extends Component {
   render() {
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header>
+          <nav>
+            <Link to='/'>Landing</Link>
+            <Link to='/roomlist'>Rooms</Link>
+          </nav>
+          <h1>Bloc Chat</h1>
         </header>
+        <main>
+          <Route exact path="/" component={Landing} />
+          <Route
+            path='/roomlist'
+            render={(props) => <RoomList {...props} firebase={firebase} />}
+          />
+        </main>
       </div>
     );
   }
