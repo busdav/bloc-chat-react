474df63dfabb962fafa0d8223696aa11e294107d
David Buser <busdav@outlook.com>
2018-12-31 10:08:05 +0100

    Complete create chat rooms

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -11,6 +11,8 @@ class RoomList extends Component {
     }
 
     this.roomsRef = this.props.firebase.database().ref('rooms');
+    this.handleChange = this.handleChange.bind(this);
+    this.createRoom = this.createRoom.bind(this);
   }
 
   componentDidMount() {
@@ -23,10 +25,12 @@ class RoomList extends Component {
 
   handleChange(e) {
     // e.preventDefault();
-    this.setState({
-      [e.target.name]: e.target.value,
-      // creator: this.props.user
-    });
+    const newName = e.target.value;
+    this.setState({ name: newName });
+    // this.setState({
+    //   [e.target.name]: e.target.value,
+    //   creator: this.props.user
+    // });
   }
 
   validateRoomName(str) {
@@ -48,28 +52,31 @@ class RoomList extends Component {
   render() {
 
     const roomForm = (
-      <form onSubmit={this.createRoom}>
+      <form className="form-inline my-2 my-lg-0" onSubmit={this.createRoom}>
         <div className="form-group">
-          <label for="name">Room name</label>
-          <input type="text" className="form-control" name="name" value={this.state.name} placeholder="New Room" onChange={this.handleChange} />
+          <input type="text" className="form-control mr-sm-2" name="name" value={this.state.name} placeholder="New Room" onChange={this.handleChange} />
         </div>
-        <button type="submit" className="btn btn-primary">Create</button>
+        <button type="submit" className="btn btn-outline-primary my-2 my-sm-0">Create</button>
       </form>
     );
 
-
     return (
-      <section className="room-list">
-        {
-          this.state.rooms.map((room) =>
-            <ul class="nav nav-pills pull-left">
-              <li class="nav-item">
-                <a class="nav-link" href="#" key={room.key}>{room.name}</a>
-              </li>
-            </ul>
-          )
-        }
-      </section>
+      <div className="room-column">
+        <section className="create-room">
+          {roomForm}
+        </section>
+        <section className="room-list">
+          {
+            this.state.rooms.map((room) =>
+              <ul className="nav nav-pills pull-left">
+                <li className="nav-item">
+                  <a className="nav-link" href="#" key={room.key}>{room.name}</a>
+                </li>
+              </ul>
+            )
+          }
+        </section>
+      </div>
     );
   }
 }
